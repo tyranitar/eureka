b0371be4fa144f7d94c4692a5289f58abeb5076a
Tyron Jung <tyronjung@hotmail.com>
2017-11-19 02:48:57 -0500

    Login

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './containers/home/Home';
+import Login from './containers/login/Login';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <MuiThemeProvider>
+                    <Router>
+                        <div>
+                            <Route exact path="/" component={ Home } />
+                            <Route path="/login" component={ Login } />
+                        </div>
+                    </Router>
+                </MuiThemeProvider>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ client/src/containers/login/Login.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import { TextField, IconButton, RaisedButton } from 'material-ui';
+import Fingerprint from 'material-ui/svg-icons/action/fingerprint';
+
+import './Login.css';
+
+const getIconButtonProps = (primaryColor) => {
+    return {
+        style: {
+            margin: '25px',
+            width: '150px',
+            height: '150px',
+            padding: '20px',
+            border: `5px solid ${primaryColor}`,
+            borderRadius: '50%',
+        },
+
+        iconStyle: {
+            width: '100px',
+            height: '100px',
+            fill: `${primaryColor}`,
+        },
+    };
+};
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <Card className="login-card">
+                    <CardText>
+                        <IconButton { ...getIconButtonProps(this.props.muiTheme.palette.primary1Color) }>
+                            <Fingerprint />
+                        </IconButton>
+                    </CardText>
+                    <CardTitle className="login-card-title" style={{ color: this.props.muiTheme.palette.primary1Color }}>
+                        Capstone
+                    </CardTitle>
+                    <CardText>
+                        <TextField ref="email" hintText="Email" />
+                        <TextField ref="password" hintText="Password" type="password" />
+                    </CardText>
+                    <CardActions className="login-card-actions">
+                        <RaisedButton label="Login" primary={ true } fullWidth={ true } />
+                    </CardActions>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default muiThemeable()(Login);
