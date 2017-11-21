350314054af6c00d76b19d46cbc6271b48a5bb52
Tyron Jung <tyronjung@hotmail.com>
2017-11-21 18:59:26 -0500

    Change banner

--- client/src/containers/search/SearchResults.js
+++ client/src/containers/search/SearchResults.js
@@ -61,14 +61,14 @@ const renderSpinner = () => {
 
 const renderResults = ({ firstSearchPerformed, searching, results, onClick, onBannerClick }) => {
     // TODO: Remove this.
-    return mapResultsToSearchResults([{
-        title: 'Software developers',
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada pretium dolor, vestibulum dictum sem ornare nec. Nunc egestas fermentum ex quis sagittis. Aliquam ultrices est hendrerit malesuada pulvinar. Nullam mattis felis ut nisl commodo, quis aliquam est placerat. Aenean luctus iaculis congue. Nam iaculis ac erat et luctus. Integer feugiat lacus ipsum, quis faucibus neque eleifend laoreet. Donec posuere nisl in lacus placerat, id vehicula leo ornare. Vivamus nec libero et sapien semper dapibus sed vel erat. Aliquam erat volutpat. Integer suscipit, nisl non pulvinar commodo, enim tellus tristique lacus, luctus dapibus ligula turpis eu magna. Etiam lobortis tristique congue. Ut sem risus, commodo sed purus a, interdum tincidunt elit. Integer vulputate sagittis quam, vitae tempor nulla pretium tincidunt.",
-        salary: 100000,
-        outlook: 'Good Outlook',
-        education: 'Bachelor\'s',
-        favorited: true,
-    }], onClick);
+    // return mapResultsToSearchResults([{
+    //     title: 'Software developers',
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada pretium dolor, vestibulum dictum sem ornare nec. Nunc egestas fermentum ex quis sagittis. Aliquam ultrices est hendrerit malesuada pulvinar. Nullam mattis felis ut nisl commodo, quis aliquam est placerat. Aenean luctus iaculis congue. Nam iaculis ac erat et luctus. Integer feugiat lacus ipsum, quis faucibus neque eleifend laoreet. Donec posuere nisl in lacus placerat, id vehicula leo ornare. Vivamus nec libero et sapien semper dapibus sed vel erat. Aliquam erat volutpat. Integer suscipit, nisl non pulvinar commodo, enim tellus tristique lacus, luctus dapibus ligula turpis eu magna. Etiam lobortis tristique congue. Ut sem risus, commodo sed purus a, interdum tincidunt elit. Integer vulputate sagittis quam, vitae tempor nulla pretium tincidunt.",
+    //     salary: 100000,
+    //     outlook: 'Good Outlook',
+    //     education: 'Bachelor\'s',
+    //     favorited: true,
+    // }], onClick);
 
     if (searching) {
         return renderSpinner();
