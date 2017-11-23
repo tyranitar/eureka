c030c666f7e16d250537441f8e0c0bd1b7962f4d
Tyron Jung <tyronjung@hotmail.com>
2017-11-23 01:28:15 -0500

    More setup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import questionnaire from './questionnaire-reducer';
+import career from './career-reducer';
 import search from './search-reducer';
 import login from './login-reducer';
 
 const reducers = combineReducers({
     router: routerReducer,
     questionnaire,
+    career,
     search,
     login,
 });
