618ed4b441169fa120fdd5888c9bd2e2264523d8
Tyron Jung <tyronjung@hotmail.com>
2017-11-29 06:53:41 -0500

    Fix questionnaire background

--- client/src/views/questionnaire/Questionnaire.js
+++ client/src/views/questionnaire/Questionnaire.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Questions from '../../containers/questionnaire/Questions';
+import { getPublicUrl } from '../../utils/common';
 
 const Questionnaire = () => (
     <div>
         <Questions />
-        <div className="background"></div>
+        <div className="background" style={{
+            backgroundImage: `url('${ getPublicUrl('/images/background.jpg') }')`,
+        }}></div>
     </div>
 );
 
