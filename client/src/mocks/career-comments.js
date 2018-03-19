b4b722b92248eacbf322096f78cb2fe1f0e89044
Tyron Jung <tyronjung@hotmail.com>
2018-03-19 00:44:40 -0400

    2 to 3 comments

--- client/src/mocks/career-comments.js
+++ client/src/mocks/career-comments.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import loremIpsum from './lorem-ipsum';
 import users from './users';
 
-export const getCareerComments = () => (_.sampleSize(users, _.random(1, 3)).map((user, idx) => ({
+export const getCareerComments = () => (_.sampleSize(users, _.random(2, 3)).map((user, idx) => ({
     user,
     content: _.sample(loremIpsum),
     createdAt: new Date(),
