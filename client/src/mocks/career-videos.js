225dd41a0cf6effe4ab0da23c3638fe7cc86f144
Tyron Jung <tyronjung@hotmail.com>
2018-03-19 00:42:55 -0400

    Video links

--- client/src/mocks/career-videos.js
+++ client/src/mocks/career-videos.js
@@ -1,21 +1,33 @@
 export default [{
     src: 'https://www.youtube.com/embed/9oWOsocN7qg',
 }, {
-    src: 'https://www.youtube.com/embed/9oWOsocN7qg',
+    src: 'https://www.youtube.com/embed/p1xntUXRhSE',
 }, {
-    src: 'https://www.youtube.com/embed/9oWOsocN7qg',
+    src: 'https://www.youtube.com/embed/NdbbcO35arw',
 }, {
-    src: 'https://www.youtube.com/embed/9oWOsocN7qg',
+    src: 'https://www.youtube.com/embed/G11t6XAIce0',
 }, {
-    src: 'https://www.youtube.com/embed/9oWOsocN7qg',
+    src: 'https://www.youtube.com/embed/zDZFcDGpL4U',
 }, {
-    src: 'https://www.youtube.com/embed/9oWOsocN7qg',
+    src: 'https://www.youtube.com/embed/-FkvBA3U5lg',
 }, {
-    src: 'https://www.youtube.com/embed/9oWOsocN7qg',
+    src: 'https://www.youtube.com/embed/VO6XEQIsCoM',
 }, {
-    src: 'https://www.youtube.com/embed/9oWOsocN7qg',
+    src: 'https://www.youtube.com/embed/sE8wg04I4U4',
 }, {
-    src: 'https://www.youtube.com/embed/9oWOsocN7qg',
+    src: 'https://www.youtube.com/embed/AcIME6dRMNU',
 }, {
-    src: 'https://www.youtube.com/embed/9oWOsocN7qg',
+    src: 'https://www.youtube.com/embed/AH1FN_y8IP8',
+}, {
+    src: 'https://www.youtube.com/embed/H91JDqeR_jg',
+}, {
+    src: 'https://www.youtube.com/embed/veriqDHLXsw',
+}, {
+    src: 'https://www.youtube.com/embed/SawYdOz1Wa8',
+}, {
+    src: 'https://www.youtube.com/embed/4D0fLisyjBY',
+}, {
+    src: 'https://www.youtube.com/embed/82aa881dpr0',
+}, {
+    src: 'https://www.youtube.com/embed/Xw0GAInXkhA',
 }];
